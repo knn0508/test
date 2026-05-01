18eab307b3c247b63cc65b97fe1080b3984eba1d
knn0508 <kanan.salmanli.i@student.aztu.edu.az>
2026-05-02 02:27:14 +0400

    Fix infra apply

--- infra/terraform/modules/keyvault/main.tf
+++ infra/terraform/modules/keyvault/main.tf
@@ -1,5 +1,25 @@
 locals {
-  compact_suffix = replace(var.suffix, "-", "")
+  compact_suffix                     = replace(var.suffix, "-", "")
+  access_policy_principal_ids        = merge(var.secret_reader_principal_ids, var.certificate_manager_principal_ids)
+  certificate_manager_principal_keys = keys(var.certificate_manager_principal_ids)
+  secret_reader_permissions = [
+    "Get",
+    "List",
+  ]
+  certificate_manager_secret_permissions = [
+    "Delete",
+    "Get",
+    "List",
+    "Set",
+  ]
+  certificate_manager_permissions = [
+    "Create",
+    "Delete",
+    "Get",
+    "Import",
+    "List",
+    "Update",
+  ]
 }
 
 resource "azurerm_key_vault" "main" {
@@ -29,40 +49,19 @@ resource "azurerm_key_vault_access_policy" "deployer" {
 }
 
 resource "azurerm_key_vault_access_policy" "secret_readers" {
-  for_each = var.secret_reader_principal_ids
+  for_each = local.access_policy_principal_ids
 
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = var.tenant_id
   object_id    = each.value
 
-  secret_permissions = [
-    "Get",
-    "List",
-  ]
+  secret_permissions      = contains(local.certificate_manager_principal_keys, each.key) ? local.certificate_manager_secret_permissions : local.secret_reader_permissions
+  certificate_permissions = contains(local.certificate_manager_principal_keys, each.key) ? local.certificate_manager_permissions : null
 }
 
-resource "azurerm_key_vault_access_policy" "certificate_managers" {
-  for_each = var.certificate_manager_principal_ids
-
-  key_vault_id = azurerm_key_vault.main.id
-  tenant_id    = var.tenant_id
-  object_id    = each.value
-
-  secret_permissions = [
-    "Delete",
-    "Get",
-    "List",
-    "Set",
-  ]
-
-  certificate_permissions = [
-    "Create",
-    "Delete",
-    "Get",
-    "Import",
-    "List",
-    "Update",
-  ]
+moved {
+  from = azurerm_key_vault_access_policy.certificate_managers["ops"]
+  to   = azurerm_key_vault_access_policy.secret_readers["ops"]
 }
 
 resource "azurerm_key_vault_secret" "sql_admin_password" {
